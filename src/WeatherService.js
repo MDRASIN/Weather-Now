676573ed9eee38e8921191e718910727d9386eb6
Mohamed Rasin <mohamedrasin24@gmail.com>
2025-10-28 10:43:28 +0530

    Update WeatherService.js

--- src/WeatherService.js
+++ src/WeatherService.js
@@ -1,6 +1,4 @@
-// src/WeatherService.js
 
-// Function to get coordinates for a given city using Open-Meteo's geocoding API
 export async function getCoordinates(city) {
   const response = await fetch(
     `https://geocoding-api.open-meteo.com/v1/search?name=${city}&count=1`
@@ -13,7 +11,6 @@ export async function getCoordinates(city) {
   return { latitude, longitude, name, country };
 }
 
-// Function to get weather data using Open-Meteo API
 export async function getWeatherData(latitude, longitude) {
   const response = await fetch(
     `https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&current=temperature_2m,relative_humidity_2m,wind_speed_10m,weather_code&daily=temperature_2m_max,temperature_2m_min,weather_code&forecast_days=5&timezone=auto`
